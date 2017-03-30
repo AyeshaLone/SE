b6f4db0db1550884cfd4e96f969c7003c0887c2e
Saim Salman <saimsalman36@hotmail.com>
2017-03-30 10:13:15 +0500

    added second commit

--- helloworld.cc
+++ helloworld.cc
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main() {
 	cout<<"hello world\n";
+	cout<<"saad is bored"
+	// string x[10];
+	// x[11];
 	return 0;
 }
+// git add -A 
+// ttracks all files
+// stages all the changes
+
+// git add .
+// new modified files are tracked
+
+// git add -u
+// tracks deleted files but not new fileve mutt eh new changes
+ // git commit give it a descirptie essage about new changes
+//
